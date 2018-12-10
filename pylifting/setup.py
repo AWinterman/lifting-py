80bd461c37bee8778e24bf4b7f4fd929118698b5
Andrew Winterman <andywinterman@gmail.com>
2018-12-10 12:37:24 -0800

    prepping to run it

--- pylifting/setup.py
+++ pylifting/setup.py
@@ -1,24 +1,34 @@
-import os
-from setuptools import setup, find_packages
+from setuptools import setup
 
 
 setup(
-    name = "lift",
-    version = "1.0",
-    author = "Andrew Winterman",
-    description = ("Lifting stats and stuff"),
+    name="lift",
+    version="1.0",
+    author="Andrew Winterman",
+    description=("Lifting stats and stuff"),
     long_description='lifting log tracking via a postgres/cli/',
-    author_email = "anywinterman@gmail.com",
-    license = "BSD",
-    keywords = "lifting cli records",
-    url = "http://packages.python.org/an_example_pypi_project",
+    author_email="anywinterman@gmail.com",
+    license="BSD",
+    keywords="lifting cli records",
+    url="http://packages.python.org/an_example_pypi_project",
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Topic :: Utilities",
         "License :: OSI Approved :: BSD License",
     ],
 
-    entry_points={'console_scripts': ['lift=pylifting.cli:cli']},
+    entry_points={
+        'console_scripts': [
+            'lift=lift.cli:cli',
+            'lift-api=lift.api:run'
+        ]
+    },
     packages=['lift'],
-    package_dir={'lift': 'src/lift'},
+    package_dir={'lift': 'lift'},
+    setup_requires=[
+       'pytest-runner',
+    ],
+    tests_require=[
+        'pytest',
+    ],
 )
